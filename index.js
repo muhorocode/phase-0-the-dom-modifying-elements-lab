94c83c01d0415bf51add0cf92f42f279d6d342ec
Elijah Kamanga <elijahkamanga1@gmail.com>
2025-07-10 18:41:22 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your code here!
+// const element=document.createElement('div')
+// document.body.append(element)
+// const ul=document.createElement('ul')
+// for(let i=0; i<3;i++){
+//     const li=document.createElement('li')
+//     li.textContent=(i+1).toString()
+//     ul.append(li)
+// }
+// element.append(ul)
+
+// const main=document.getElementById('main')
+// main.innerHTML="<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+
+// const elements=document.getElementById('main')
+// elements.style.height='300px'
+// elements.style.backgroundColor='#27647B'
+
+// elements.textContent='You\'ve changed what\'s on the screen!'
+// elements.style.fontSize='24px'
+// elements.style.marginLeft='30px'
+// elements.style.marginRight='30px'
+// elements.style.lineHeight=2
+
+// elements.className='pet-listing dog'
+// elements.classList.remove('dog')
+// elements.classList.add('cat','sale')
+
+// const uls=   document.getElementsByTagName('ul'[0])
+// const secondChild=ul.querySelector('li:nth-child(2)')
+// ul.removeChild(secondChild)
+
+// ul.remove()
+
+main.remove()
+const newHeader=document.createElement('h1')
+newHeader.id='victory'
+newHeader.textContent='Elijah Kamanga is the champion'
+document.body.append(newHeader)
